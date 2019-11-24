3884f363d36c499cc70024d2516101314bd25a3a
sag754 <56234238+sag754@users.noreply.github.com>
2019-11-23 22:02:57 -0500

    Added Dialogue Box, Continue, and Start buttons and functions.

---
+++ UI Text Game/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public GameObject startButton;
+    public GameObject dialogueBox;
+    public GameObject continueButton;
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        startButton.SetActive(false);
+        dialogueBox.SetActive(true);
+        continueButton.SetActive(true);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
